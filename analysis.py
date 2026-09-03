031506d5caea5167359905431a46dcf7d6976fb7
Imene Bouretal <im97en@gmail.com>
2026-09-03 23:55:53 +0100

    New update

--- analysis.py
+++ analysis.py
@@ -1,2 +1,20 @@
 import pandas as pd
 
+# Load the data
+df = pd.read_excel("OnlineRetail.xlsx")
+
+"""
+
+# Inspecting the data 
+print(df.head(5))
+print(df.shape)
+print(df.columns)
+print(df.describe())
+print(df.info())
+print(df.isna().sum())
+print(df[df["Quantity"] < 0].head())
+"""
+
+
+print(df["InvoiceNo"].astype(str).str.startswith("C").sum())
+print(df[df["InvoiceNo"].astype(str).str.startswith("C")]["Quantity"].describe())
